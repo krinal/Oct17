981d90252b06b708a2f80aefd78291a308b9fbeb
Krinal Manakiwala <krinalmanakiwala@gmail.com>
2013-10-16 23:35:21 -0400

    Initial Commit

---
+++ Oct17/Oct17AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct17AppDelegate.h
+//  Oct17
+//
+//  Created by Krinal Manakiwala on 10/16/13.
+//  Copyright (c) 2013 Computerlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct17AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Oct17/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct17
+//
+//  Created by Krinal Manakiwala on 10/16/13.
+//  Copyright (c) 2013 Computerlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Oct17AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Oct17AppDelegate class]));
+    }
+}
